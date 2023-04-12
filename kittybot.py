e68b4122d002eeb50fe494191dfa7bc2d2490fc5
Виктория Сопина <vt.sopina@yandex.ru>
2023-04-12 19:34:36 +0300

    Add logger

--- kittybot.py
+++ kittybot.py
@@ -1,23 +1,38 @@
-import requests
+import logging
 import os
 
-from dotenv import load_dotenv
-from telegram import Bot, ReplyKeyboardMarkup
+import requests
+
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
+from dotenv import load_dotenv
 
 
 load_dotenv()
 
-token = os.getenv('TOKEN')
+
+secret_token = os.getenv('TOKEN')
 chat_sid = os.getenv('CHAT_ID')
-bot = Bot(token=token)
-updater = Updater(token=token)
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+
 URL = 'https://api.thecatapi.com/v1/images/search'
-text = 'Вам телеграмма!'  # мб потом сюда дописать словарь?
 
 
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
@@ -46,17 +61,17 @@ def wake_up(update, context):
     context.bot.send_photo(chat.id, get_new_image())
 
 
-def send_message(message):
-    # Обрати внимение, это тест отправки из кода, в собственный (chat_sid) чат
-    Bot.send_message(chat_sid, message)
+def main():
+    updater = Updater(token=secret_token)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+    updater.start_polling(poll_interval=10.0)
+
+    updater.idle()
 
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
-
-updater.start_polling(poll_interval=10.0)  # переодичность, в секундах флоат
-
-updater.idle()
+if __name__ == '__main__':
+    main()
